8a3df7709e6c1e8dec77903fed46b4e277c6fdf8
imon <service@yimmr.com>
2023-04-22 16:52:04 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: ImpackWP
+ * Description: 核心基础功能包
+ * Version: 0.1.0
+ * Requires at least: 5.2
+ * Requires PHP: 7.4
+ * Author: imoncn
+ * Author URI: https://wp.yimmr.com/
+ * License: MIT
+ * License URI: https://github.com/yimmr/wordpress/blob/c0e4f4a8a1de7c0a41d691a1ec9c96be5f61583f/LICENSE
+ * Text Domain: impackwp
+ * Domain Path: /languages
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
